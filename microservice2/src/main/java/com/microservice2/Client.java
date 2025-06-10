45b344cc39021fd6b614cb1b8d2e45c7ff3fdcba
Subham919 <subhamkumar832@gmail.com>
2025-06-10 10:43:50 +0530

    HMS-204-microservice2-microservice

---
+++ microservice2/src/main/java/com/microservice2/Client.java
@@ -0,0 +1,12 @@
+package com.microservice2;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "MICROSERVICE-1")
+public interface Client {
+
+    @GetMapping("/message")
+    public String getData();
+
+}
